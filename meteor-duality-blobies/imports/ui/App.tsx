a9d972c5421aeec1543a041c7fbb6b8dd6d4eaee
Tomi Kokkonen <tomi@valohai.com>
2022-01-29 17:01:33 +0200

    remove broken link file

--- meteor-duality-blobies/imports/ui/App.tsx
+++ meteor-duality-blobies/imports/ui/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Hello } from './Hello';
-import { Info } from './Info';
 import {GameComponent} from "/imports/ui/Game";
 import {startNewGame} from "/imports/control/game-logic";
 
@@ -8,7 +7,6 @@ export const App = () => (
   <div className={"container-fluid"}>
     <h1>Welcome to Meteor!</h1>
     <Hello />
-    <Info />
     <GameComponent game={startNewGame()} />
   </div>
 );
