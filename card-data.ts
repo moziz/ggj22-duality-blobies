0fb9fc8f7e7b66d93848f644fa191075051e45b5
Tomi Kokkonen <tomi@valohai.com>
2022-01-29 12:57:57 +0200

    Add card data file

---
+++ card-data.ts
@@ -0,0 +1,23 @@
+// Korttipeli data
+
+type Side = "Dino"|"Cat"
+
+type EffectTriggerPhase: "PlayFirst" | "Play" | "Resolve" | "Discard"
+
+type EffectType: "None" | "Draw" | "AddPower" 
+ 
+
+export interface ComboEffect{
+	trigger: EffectTriggerPhase,
+	activeInHand: boolean,
+	effectType: EffectType,
+	effectArgs: {[key:string]:any},
+	comboNeed: "NoNeed" | "OtherSame" | "OtherDifferent",
+}
+
+export interface Card{
+	name:string,
+	power:number,
+	side:Side,
+	effects:ComboEffect[],
+}
