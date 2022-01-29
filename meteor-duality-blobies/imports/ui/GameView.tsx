b5261146cf057a7a3df1925539667ecc54744603
Tomi Kokkonen <tomi@valohai.com>
2022-01-30 00:16:58 +0200

    Why u no work?

--- meteor-duality-blobies/imports/ui/GameView.tsx
+++ meteor-duality-blobies/imports/ui/GameView.tsx
@@ -1,16 +1,20 @@
-import React, {useState} from "react";
-import { useParams } from "react-router-dom";
-import { useTracker } from 'meteor/react-meteor-data'
-import {GameCollection} from "/imports/data/game";
+import React from "react";
+import {useParams} from "react-router-dom";
+import {useTracker} from 'meteor/react-meteor-data'
+import {Game, GameCollection} from "/imports/data/game";
 
-import { Meteor } from "meteor/meteor";
+import {Meteor} from "meteor/meteor";
 import {GameComponent} from "/imports/ui/GameComponent";
-import {startNewGame} from "/imports/control/game-logic";
+import {drawPhase, handlePurchase, playCardInGame, startNewGame} from "/imports/control/game-logic";
+import {Card} from "/imports/data/card-data";
+import {PlayerID} from "/imports/data/player";
+import {cloneDeep, isEqual} from "lodash";
 
-const useGame = (gameId:string = "") => useTracker(() => {
+const useGame = (gameId: string = "") => useTracker(() => {
     const subscription = Meteor.subscribe('games', gameId)
-    let gameObject = GameCollection.findOne({ _id: gameId })
-    if(gameObject === undefined) {
+    let gameObject = GameCollection.findOne({_id: gameId})
+    if (gameObject === undefined && subscription.ready()) {
+        console.log("new game")
         gameObject = startNewGame()
         gameObject.name = gameId;
         GameCollection.upsert({_id: gameId}, gameObject)
@@ -18,26 +22,62 @@ const useGame = (gameId:string = "") => useTracker(() => {
     return {
         gameObject: gameObject,
         isLoading: !subscription.ready(),
-        gameId: gameId
+        gameId: gameId,
     }
 }, [gameId])
 
 export const GameView: React.FC = () => {
 
-    let { gameId } = useParams();
-    const { isLoading, gameObject } = useGame(gameId)
+    let {gameId} = useParams();
+    console.log("id", gameId);
+    const {isLoading, gameObject} = useGame(gameId);
 
-    if(!isLoading) {
-        if(gameObject != undefined) {
-            return (
-                <GameComponent game={gameObject} />
-            );
-        }
-        else {
-            return <div>no game found</div>
-        }
+    const [tmpGame, setTMPGame] = React.useState(gameObject);
+
+    if (!isEqual(gameObject, tmpGame)) {
+        setTMPGame(gameObject);
     }
-    else {
+
+    const setGame = React.useCallback((game: Game) => {
+        GameCollection.upsert({_id: gameId}, game)
+        setTMPGame(game);
+    }, [gameId])
+
+    const playCard = React.useCallback((c: Card, p: PlayerID) => {
+        if (!tmpGame) return;
+        const game = cloneDeep(tmpGame);
+        playCardInGame(game, c, p);
+        setGame(game);
+    }, [setGame, tmpGame])
+
+    const toDrawState = React.useCallback(() => {
+        if (!tmpGame) return;
+        const game = cloneDeep(tmpGame);
+        drawPhase(game);
+        setGame(game);
+    }, [setGame, tmpGame, setTMPGame])
+
+    const onPurchase = React.useCallback((c: Card, p: PlayerID) => {
+        if (!tmpGame) return;
+        const game = cloneDeep(tmpGame);
+        handlePurchase(game, c, p);
+        setGame(game);
+    }, [setGame, tmpGame])
+
+
+    if (!isLoading) {
+        if (gameObject && tmpGame) {
+            return (
+                <div className={"container-fluid"}>
+                    <h1>Doality Blobies</h1>
+                    <GameComponent game={tmpGame} toDrawState={toDrawState} playCard={playCard}
+                                   purchaseCard={onPurchase}/>
+                </div>
+            );
+        } else {
+            return <div>no game found!</div>
+        }
+    } else {
         return (
             <h1>LOADING</h1>
         )
